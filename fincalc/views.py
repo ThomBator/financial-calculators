6f535b3f886bbcc019f139a73841940ff2c5b598
ThomBator <thom.bator@gmail.com>
2023-01-10 20:19:29 -0500

    noticed results were showing up hidden under nav on some screen sizes so adjusted

--- fincalc/views.py
+++ fincalc/views.py
@@ -15,25 +15,25 @@ def index():
 def loan():
     if request.method == 'POST':
         if 'reset' in request.form:
-                return redirect(url_for('views.loan'))  
-        error = None 
+                return redirect(url_for('views.loan'))
+        error = None
         if not request.form['loanAmount']:
             error = "Loan amount is required"
         elif (not request.form['years'] and not request.form['months']):
             error = "You must enter a minimum of 1 month or 1 year with a value greater than 0."
-       
+
         elif not request.form['interestRate'] or float(request.form['interestRate']) <= 0:
             error = "Please enter an interest rate greater than 0"
         elif not request.form['payFrequency']:
             error = "Please select your payment frequency"
-        if error is not None: 
+        if error is not None:
             flash('Error: ' + error)
             return redirect(url_for('views.loan'))
         else:
             loan_amount = float(request.form['loanAmount'])
             interest_rate = float(request.form['interestRate'])
             if len(request.form['years']) == 0:
-                years = 0 
+                years = 0
             else:
                 years = int(request.form['years'])
             if len(request.form['months']) == 0:
@@ -45,18 +45,18 @@ def loan():
                 flash("Error: " + error)
                 return redirect(url_for('views.loan'))
             pay_frequency = request.form['payFrequency']
-            
+
             total_months = (years * 12) + months
             loan_details = loan_calculator(loan_amount, interest_rate, pay_frequency, total_months)
             loan_chart = pie_chart(loan_amount, float(loan_details["interest_paid"]))
-                          
+
             return render_template('loan.html', LOAN_DETAILS = loan_details, SUBMIT = True, LOAN_CHART = loan_chart)
     return render_template('loan.html', SUBMIT = False)
-                        
+
 @bp.route('/invest', methods=('POST','GET'))
 def invest():
     if request.method == 'POST':
-        
+
         if 'reset' in request.form:
                 return redirect(url_for('views.invest'))
         error = None
@@ -65,10 +65,10 @@ def invest():
         elif not request.form['interestRate']:
             error = 'You must enter an interest rate greater than 0.'
         elif not request.form['years']:
-            error = 'You must enter a number of years greater than 0.' 
-        if error is not None: 
+            error = 'You must enter a number of years greater than 0.'
+        if error is not None:
             flash('Error: ' + error)
-            return redirect(url_for('views.invest'))  
+            return redirect(url_for('views.invest'))
         else:
 
             if len(request.form['initialDeposit']) == 0:
@@ -77,28 +77,28 @@ def invest():
                 initial_deposit = float(request.form['initialDeposit'])
             if len(request.form['monthlyDeposit']) == 0:
                 monthly_deposit = 0
-            else:    
+            else:
                 monthly_deposit = float(request.form['monthlyDeposit'])
-            if initial_deposit == 0 and monthly_deposit == 0: 
+            if initial_deposit == 0 and monthly_deposit == 0:
                 error = 'You must enter a value greater than 0 for either Initial Deposit or Monthtly Deposit.'
                 flash('Error: ' + error)
-                return redirect(url_for('views.invest'))  
-                
+                return redirect(url_for('views.invest'))
 
-            
-            
-            interest_rate = float(request.form['interestRate']) 
+
+
+
+            interest_rate = float(request.form['interestRate'])
             years = int(request.form['years'])
-            invest_details = invest_calculator(initial_deposit, monthly_deposit, interest_rate, years) 
+            invest_details = invest_calculator(initial_deposit, monthly_deposit, interest_rate, years)
             yearly_returns = invest_details["returns_each_year"]
             invest_graph = line_graph(yearly_returns, years)
             return render_template('invest.html', INVEST_DETAILS = invest_details, SUBMIT = True, INVEST_GRAPH = invest_graph)
 
     return render_template('invest.html', SUBMIT = False)
 
-def loan_calculator(loan_amount: float, interest_rate: float, pay_frequency: str, total_months: int) -> dict: 
+def loan_calculator(loan_amount: float, interest_rate: float, pay_frequency: str, total_months: int) -> dict:
     if pay_frequency == 'monthly':
-        period_interest = interest_rate / 1200 
+        period_interest = interest_rate / 1200
         total_periods = total_months
     elif pay_frequency == 'bi-weekly':
         period_interest = interest_rate /2600
@@ -111,16 +111,16 @@ def loan_calculator(loan_amount: float, interest_rate: float, pay_frequency: str
         /(1-(pow(1+ period_interest, -total_periods)))
     )
     period_payment = round(total_loan_cost / total_periods, 2)
-    interest_paid = round(total_loan_cost - loan_amount,2)  
-    total_loan_cost = round(total_loan_cost, 2) 
+    interest_paid = round(total_loan_cost - loan_amount,2)
+    total_loan_cost = round(total_loan_cost, 2)
     loan_details = {"total_loan_cost": total_loan_cost, "period_payment": period_payment, "interest_paid": interest_paid, "period_type": pay_frequency}
-    return loan_details 
+    return loan_details
 
 def pie_chart(total_loan : float, interest_paid: float) -> base64:
     pie_list = [total_loan, interest_paid] #list of values for pie chart
     pie_labels = ["Principal", "Interest"]
     colors = sns.color_palette("Paired")[0:2]
-    fig = Figure() 
+    fig = Figure()
     ax = fig.subplots()
     fig.set_facecolor("#f9f8f7")
     ax.pie(pie_list, labels=pie_labels,colors = colors, autopct='%.1f%%', explode = [0, .025], )
@@ -128,12 +128,12 @@ def pie_chart(total_loan : float, interest_paid: float) -> base64:
     fig.savefig(buf, format="png", bbox_inches='tight', pad_inches=0)
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return data
-    
-def invest_calculator(initial_deposit: float, monthly_deposit: float, avg_annual_rate: float, years: int) -> dict: 
+
+def invest_calculator(initial_deposit: float, monthly_deposit: float, avg_annual_rate: float, years: int) -> dict:
     #calculation will be done with monthly compounding so years need to be converted
     initial_deposit_return = initial_deposit * pow((1 + avg_annual_rate / 100), years)
     '''
-    for the monthly deposits, we are assuming annual compounding to keep things simple, so _monthly_deposit will be multiplied by 12. Credit to https://www.wallstreetiswaiting.com/running-the-numbers-1/calculating-interest-recurring-payments/ for the math regarding monthly contributions. 
+    for the monthly deposits, we are assuming annual compounding to keep things simple, so _monthly_deposit will be multiplied by 12. Credit to https://www.wallstreetiswaiting.com/running-the-numbers-1/calculating-interest-recurring-payments/ for the math regarding monthly contributions.
     '''
     monthly_deposits_return = (
         monthly_deposit * ((pow((1 +avg_annual_rate / 100), years)-1)/(avg_annual_rate / 100)) * 12
@@ -148,23 +148,23 @@ def invest_calculator(initial_deposit: float, monthly_deposit: float, avg_annual
         monthly_deposit * ((pow((1 +avg_annual_rate / 100), i)-1)/(avg_annual_rate / 100)) * 12
     )
         returns_each_year.append(initial_deposit_yearly + monthly_deposits_yearly)
-    
+
     total = initial_deposit_return + monthly_deposits_return
     total_deposits = initial_deposit + (monthly_deposit * years * 12)
-    # A list of yearly values can also be used to create an informative graph 
+    # A list of yearly values can also be used to create an informative graph
     total = round(total, 2)
-    profit = round(total - total_deposits,2) 
+    profit = round(total - total_deposits,2)
     total_str = "{:,.2f}".format(total)
     profit_str = "{:,.2f}".format(profit)
     total_deposits_str = "{:,.2f}".format(round(total_deposits, 2))
     invest_details = {"total": total_str, "years": years, "total_deposits":total_deposits_str, "profit":profit_str, "returns_each_year": returns_each_year}
     return invest_details
-    
+
 def line_graph(running_returns :list, years :int) -> base64:
 
     years_list = list(range(1, years + 1))
     colors = sns.color_palette("Paired")[0:2]
-    fig = Figure() 
+    fig = Figure()
     ax = fig.subplots()
     fig.set_facecolor("#f9f8f7")
     ax.plot(years_list, running_returns)
@@ -173,4 +173,3 @@ def line_graph(running_returns :list, years :int) -> base64:
     fig.savefig(buf, format="png", bbox_inches='tight', pad_inches=0)
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return data
-
